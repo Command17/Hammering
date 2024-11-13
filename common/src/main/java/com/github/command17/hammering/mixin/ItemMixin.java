c2371ac1146a551eb7c32a8e2a5f602622ce0063
Command17 <84639718+Command17@users.noreply.github.com>
2024-11-13 16:38:28 +0100

    Add Destroy Speed Modifier

---
+++ common/src/main/java/com/github/command17/hammering/mixin/ItemMixin.java
@@ -0,0 +1,21 @@
+package com.github.command17.hammering.mixin;
+
+import com.github.command17.hammering.enchantment.effect.ModEnchantmentEffectComponents;
+import com.github.command17.hammering.util.EnchantmentUtil;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.state.BlockState;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(Item.class)
+public class ItemMixin {
+    @Inject(method = "getDestroySpeed", at = @At("RETURN"), cancellable = true)
+    private void hammering$modifyDestroySpeed(ItemStack stack, BlockState state, CallbackInfoReturnable<Float> cir) {
+        float modifier = (float) 1 / (EnchantmentUtil.getCountOfEnchantmentComponent(stack, ModEnchantmentEffectComponents.AREA_MINE.get()) + 1);
+
+        cir.setReturnValue(cir.getReturnValueF() * modifier);
+    }
+}
