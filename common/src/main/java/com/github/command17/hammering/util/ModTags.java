064da5f4b515b0c477b33f54cf3d5ebcf83df9e9
Command17 <84639718+Command17@users.noreply.github.com>
2024-11-13 16:38:55 +0100

    Update ModTags.java

--- common/src/main/java/com/github/command17/hammering/util/ModTags.java
+++ common/src/main/java/com/github/command17/hammering/util/ModTags.java
@@ -4,8 +4,17 @@ import com.github.command17.hammering.Hammering;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
 
 public class ModTags {
+    public static class BlockTags {
+        public static final TagKey<Block> MINEABLE_WITH_HAMMER = key("mineable/hammer");
+
+        private static TagKey<Block> key(String name) {
+            return TagKey.create(Registries.BLOCK, Hammering.resource(name));
+        }
+    }
+
     public static class ItemTags {
         public static final TagKey<Item> HAMMER = key("hammer");
 
