a23a408133b91b0ddecee55ffb30ce94175b71b1
Command17 <84639718+Command17@users.noreply.github.com>
2024-11-23 19:23:49 +0100

    Add convention tags

--- common/src/main/java/com/github/command17/hammering/util/ModTags.java
+++ common/src/main/java/com/github/command17/hammering/util/ModTags.java
@@ -2,24 +2,34 @@ package com.github.command17.hammering.util;
 
 import com.github.command17.hammering.Hammering;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 
 public class ModTags {
     public static class BlockTags {
-        public static final TagKey<Block> MINEABLE_WITH_HAMMER = key("mineable/hammer");
+        public static final TagKey<Block> MINEABLE_WITH_HAMMER = tag("mineable/hammer");
 
-        private static TagKey<Block> key(String name) {
+        private static TagKey<Block> tag(String name) {
             return TagKey.create(Registries.BLOCK, Hammering.resource(name));
+        }
+
+        private static TagKey<Block> conventionTag(String name) {
+            return TagKey.create(Registries.BLOCK, ResourceLocation.fromNamespaceAndPath("c", name));
         }
     }
 
     public static class ItemTags {
-        public static final TagKey<Item> HAMMER = key("hammer");
+        public static final TagKey<Item> HAMMER = tag("hammer");
+        public static final TagKey<Item> MINING_TOOLS = conventionTag("tools/mining_tools");
 
-        private static TagKey<Item> key(String name) {
+        private static TagKey<Item> tag(String name) {
             return TagKey.create(Registries.ITEM, Hammering.resource(name));
+        }
+
+        private static TagKey<Item> conventionTag(String name) {
+            return TagKey.create(Registries.ITEM, ResourceLocation.fromNamespaceAndPath("c", name));
         }
     }
 }
