7436e701f2cea6be065a562daf1eda226268e8cf
Command17 <84639718+Command17@users.noreply.github.com>
2025-01-02 18:04:51 +0100

    Fix Hammers and update to EnchantedBookLib

--- common/src/main/java/com/github/command17/hammering/item/ModItems.java
+++ common/src/main/java/com/github/command17/hammering/item/ModItems.java
@@ -1,21 +1,20 @@
 package com.github.command17.hammering.item;
 
+import com.github.command17.enchantedbooklib.api.registry.IRegistrySupplier;
+import com.github.command17.enchantedbooklib.api.registry.RegistryHelper;
 import com.github.command17.hammering.Hammering;
-import dev.architectury.registry.registries.DeferredRegister;
-import dev.architectury.registry.registries.RegistrySupplier;
-import net.minecraft.core.registries.Registries;
 import net.minecraft.world.item.*;
 
 public class ModItems {
-    private static final DeferredRegister<Item> REGISTRY = DeferredRegister.create(Hammering.MOD_ID, Registries.ITEM);
+    private static final RegistryHelper.ItemHelper REGISTRY = RegistryHelper.createItemHelper(Hammering.MOD_ID);
 
-    public static final RegistrySupplier<Item> IRON_HAMMER = createHammer("iron_hammer", Tiers.IRON, 5, -2.8f, of());
-    public static final RegistrySupplier<Item> GOLD_HAMMER = createHammer("gold_hammer", Tiers.GOLD, 5, -2.8f, of());
-    public static final RegistrySupplier<Item> DIAMOND_HAMMER = createHammer("diamond_hammer", Tiers.DIAMOND, 4, -2.8f, of());
-    public static final RegistrySupplier<Item> NETHERITE_HAMMER = createHammer("netherite_hammer", Tiers.NETHERITE, 4, -2.8f, of().fireResistant());
+    public static final IRegistrySupplier<Item> IRON_HAMMER = createHammer("iron_hammer", Tiers.IRON, 5, -3, of());
+    public static final IRegistrySupplier<Item> GOLD_HAMMER = createHammer("gold_hammer", Tiers.GOLD, 5, -3, of());
+    public static final IRegistrySupplier<Item> DIAMOND_HAMMER = createHammer("diamond_hammer", Tiers.DIAMOND, 4, -3, of());
+    public static final IRegistrySupplier<Item> NETHERITE_HAMMER = createHammer("netherite_hammer", Tiers.NETHERITE, 4, -3, of().fireResistant());
 
-    private static RegistrySupplier<Item> createHammer(String name, Tier tier, int attackDamage, float attackSpeed, Item.Properties properties) {
-        return REGISTRY.register(name, () -> new HammerItem(tier, properties.attributes(DiggerItem.createAttributes(tier, attackDamage, attackSpeed)), 2));
+    private static IRegistrySupplier<Item> createHammer(String name, Tier tier, int attackDamage, float attackSpeed, Item.Properties properties) {
+        return REGISTRY.registerItem(name, (props) -> new HammerItem(tier, props.attributes(DiggerItem.createAttributes(tier, attackDamage, attackSpeed)), 2), properties);
     }
 
     private static Item.Properties of() {
