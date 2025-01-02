6ec988c242367e2b9b6f0c06082f427874c27b91
Command17 <84639718+Command17@users.noreply.github.com>
2025-01-02 18:04:20 +0100

    Update ItemMixin.java

--- common/src/main/java/com/github/command17/hammering/mixin/ItemMixin.java
+++ common/src/main/java/com/github/command17/hammering/mixin/ItemMixin.java
@@ -15,7 +15,6 @@ public class ItemMixin {
     @Inject(method = "getDestroySpeed", at = @At("RETURN"), cancellable = true)
     private void hammering$modifyDestroySpeed(ItemStack stack, BlockState state, CallbackInfoReturnable<Float> cir) {
         float modifier = (float) 1 / (EnchantmentUtil.getCountOfEnchantmentComponent(stack, ModEnchantmentEffectComponents.AREA_MINE.get()) + 1);
-
         cir.setReturnValue(cir.getReturnValueF() * modifier);
     }
 }
