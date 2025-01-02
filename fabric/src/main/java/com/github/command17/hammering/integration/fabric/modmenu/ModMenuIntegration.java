c61f7e71780bd4be4bf07c8a6021faf496b22d9a
Command17 <84639718+Command17@users.noreply.github.com>
2025-01-02 18:05:15 +0100

    Create ModMenuIntegration.java

---
+++ fabric/src/main/java/com/github/command17/hammering/integration/fabric/modmenu/ModMenuIntegration.java
@@ -0,0 +1,12 @@
+package com.github.command17.hammering.integration.fabric.modmenu;
+
+import com.github.command17.hammering.integration.clothconfig.ClothConfigIntegration;
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
+
+public final class ModMenuIntegration implements ModMenuApi {
+    @Override
+    public ConfigScreenFactory<?> getModConfigScreenFactory() {
+        return (parent) -> ClothConfigIntegration.createConfigScreen().setParentScreen(parent).build();
+    }
+}
