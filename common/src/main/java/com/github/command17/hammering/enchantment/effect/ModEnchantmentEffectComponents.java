53eead3a2be8283d93e68d68de0b747e3b04cadb
Command17 <84639718+Command17@users.noreply.github.com>
2025-03-04 15:13:00 +0100

    Some refactoring

--- common/src/main/java/com/github/command17/hammering/enchantment/effect/ModEnchantmentEffectComponents.java
+++ common/src/main/java/com/github/command17/hammering/enchantment/effect/ModEnchantmentEffectComponents.java
@@ -7,11 +7,17 @@ import net.minecraft.core.component.DataComponentType;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.util.Unit;
 
+import java.util.function.Supplier;
+
 public final class ModEnchantmentEffectComponents {
     private static final DeferredRegister<DataComponentType<?>> REGISTRY = DeferredRegister.create(Hammering.MOD_ID, Registries.ENCHANTMENT_EFFECT_COMPONENT_TYPE);
 
-    public static final RegistrySupplier<DataComponentType<Unit>> AREA_MINE = REGISTRY.register("area_mine",
+    public static final RegistrySupplier<DataComponentType<Unit>> AREA_MINE = register("area_mine",
             () -> DataComponentType.<Unit>builder().persistent(Unit.CODEC).build());
+
+    private static<T> RegistrySupplier<DataComponentType<T>> register(String name, Supplier<DataComponentType<T>> componentType) {
+        return REGISTRY.register(name, componentType);
+    }
 
     public static void register() {
         REGISTRY.register();
--- common/src/main/java/com/github/command17/hammering/item/ModItems.java
+++ common/src/main/java/com/github/command17/hammering/item/ModItems.java
@@ -6,21 +6,27 @@ import dev.architectury.registry.registries.RegistrySupplier;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.world.item.*;
 
-public class ModItems {
+import java.util.function.Supplier;
+
+public final class ModItems {
     private static final DeferredRegister<Item> REGISTRY = DeferredRegister.create(Hammering.MOD_ID, Registries.ITEM);
 
-    public static final RegistrySupplier<Item> IRON_HAMMER = createHammer("iron_hammer", Tiers.IRON, 5, -3, of());
-    public static final RegistrySupplier<Item> GOLDEN_HAMMER = createHammer("golden_hammer", Tiers.GOLD, 5, -3, of());
-    public static final RegistrySupplier<Item> DIAMOND_HAMMER = createHammer("diamond_hammer", Tiers.DIAMOND, 4, -3, of());
-    public static final RegistrySupplier<Item> NETHERITE_HAMMER = createHammer("netherite_hammer", Tiers.NETHERITE, 4, -3, of().fireResistant());
+    public static final RegistrySupplier<Item> IRON_HAMMER = registerHammer("iron_hammer", Tiers.IRON, 5, -3, of());
+    public static final RegistrySupplier<Item> GOLDEN_HAMMER = registerHammer("golden_hammer", Tiers.GOLD, 5, -3, of());
+    public static final RegistrySupplier<Item> DIAMOND_HAMMER = registerHammer("diamond_hammer", Tiers.DIAMOND, 4, -3, of());
+    public static final RegistrySupplier<Item> NETHERITE_HAMMER = registerHammer("netherite_hammer", Tiers.NETHERITE, 4, -3, of().fireResistant());
 
-    private static RegistrySupplier<Item> createHammer(String name, Tier tier, int attackDamage, float attackSpeed, Item.Properties properties) {
-        return REGISTRY.register(name,
+    private static RegistrySupplier<Item> registerHammer(String name, Tier tier, int attackDamage, float attackSpeed, Item.Properties properties) {
+        return register(name,
                 () -> new HammerItem(
                         tier,
                         properties.attributes(DiggerItem.createAttributes(tier, attackDamage, attackSpeed)),
                         Hammering.CONFIG.hammerDurabilityMultiplier.get()
                 ));
+    }
+
+    private static RegistrySupplier<Item> register(String name, Supplier<Item> item) {
+        return REGISTRY.register(name, item);
     }
 
     private static Item.Properties of() {
