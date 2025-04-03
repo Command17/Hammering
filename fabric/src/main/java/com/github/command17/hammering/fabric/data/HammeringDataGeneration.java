de7a84337b927f02c50dffe3503c0ccb413d78a2
Command17 <84639718+Command17@users.noreply.github.com>
2025-04-03 12:55:29 +0200

    [+] Generate Languages With Data Generation

--- fabric/src/main/java/com/github/command17/hammering/fabric/data/HammeringDataGeneration.java
+++ fabric/src/main/java/com/github/command17/hammering/fabric/data/HammeringDataGeneration.java
@@ -12,5 +12,23 @@ public final class HammeringDataGeneration implements DataGeneratorEntrypoint {
         pack.addProvider(ModAdvancementProvider::new);
         pack.addProvider(ModItemTagProvider::new);
         pack.addProvider(ModBlockTagProvider::new);
+
+        // English
+        pack.addProvider((output, provider) -> new ModLanguageProviders.EnglishLanguageProvider(output, "en_us", provider));
+
+        // German
+        pack.addProvider((output, provider) -> new ModLanguageProviders.GermanLanguageProvider(output, "de_de", provider));
+
+        // Austrian
+        pack.addProvider((output, provider) -> new ModLanguageProviders.GermanLanguageProvider(output, "de_at", provider));
+
+        // Swiss German
+        pack.addProvider((output, provider) -> new ModLanguageProviders.GermanLanguageProvider(output, "de_ch", provider));
+
+        // Bavarian German
+        pack.addProvider((output, provider) -> new ModLanguageProviders.GermanLanguageProvider(output, "bar", provider));
+
+        // Saxon German
+        pack.addProvider((output, provider) -> new ModLanguageProviders.GermanLanguageProvider(output, "sxu", provider));
     }
 }
---
+++ fabric/src/main/java/com/github/command17/hammering/fabric/data/ModLanguageProviders.java
@@ -0,0 +1,72 @@
+package com.github.command17.hammering.fabric.data;
+
+import com.github.command17.hammering.Hammering;
+import com.github.command17.hammering.enchantment.ModEnchantments;
+import com.github.command17.hammering.item.ModItems;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricLanguageProvider;
+import net.minecraft.core.HolderLookup;
+
+import java.util.concurrent.CompletableFuture;
+
+public final class ModLanguageProviders {
+    private static String translationKey(String prefix, String suffix) {
+        return prefix + Hammering.MOD_ID + suffix;
+    }
+
+    public static class EnglishLanguageProvider extends FabricLanguageProvider {
+        public EnglishLanguageProvider(FabricDataOutput dataOutput, String languageCode, CompletableFuture<HolderLookup.Provider> registryLookup) {
+            super(dataOutput, languageCode, registryLookup);
+        }
+
+        @Override
+        public void generateTranslations(HolderLookup.Provider provider, TranslationBuilder builder) {
+            // Hammers
+            builder.add(ModItems.IRON_HAMMER.get(), "Iron Hammer");
+            builder.add(ModItems.GOLDEN_HAMMER.get(), "Golden Hammer");
+            builder.add(ModItems.DIAMOND_HAMMER.get(), "Diamond Hammer");
+            builder.add(ModItems.NETHERITE_HAMMER.get(), "Netherite Hammer");
+
+            // Creative Mode Tabs
+            builder.add(translationKey("itemGroup.", ".main"), "Hammers");
+
+            // Advancements
+            builder.add(translationKey("advancement.", ".hammer_time.title"), "Hammer Time!");
+            builder.add(translationKey("advancement.", ".hammer_time.info"), "Get an Iron Hammer");
+            builder.add(translationKey("advancement.", ".hammer_collection.title"), "Hammer Collection");
+            builder.add(translationKey("advancement.", ".hammer_collection.info"), "Get every Hammer");
+
+            // Enchantments
+            builder.addEnchantment(ModEnchantments.HAMMERING, "Hammering");
+            builder.add(translationKey("enchantment.", ".hammering.desc"), "Allows you to mine a large area at once.");
+        }
+    }
+
+    public static class GermanLanguageProvider extends FabricLanguageProvider {
+        public GermanLanguageProvider(FabricDataOutput dataOutput, String languageCode, CompletableFuture<HolderLookup.Provider> registryLookup) {
+            super(dataOutput, languageCode, registryLookup);
+        }
+
+        @Override
+        public void generateTranslations(HolderLookup.Provider provider, TranslationBuilder builder) {
+            // Hammers
+            builder.add(ModItems.IRON_HAMMER.get(), "Eisenhammer");
+            builder.add(ModItems.GOLDEN_HAMMER.get(), "Goldhammer");
+            builder.add(ModItems.DIAMOND_HAMMER.get(), "Diamanthammer");
+            builder.add(ModItems.NETHERITE_HAMMER.get(), "Netherithammer");
+
+            // Creative Mode Tabs
+            builder.add(translationKey("itemGroup.", ".main"), "Hämmer");
+
+            // Advancements
+            builder.add(translationKey("advancement.", ".hammer_time.title"), "Hämmer Zeit!");
+            builder.add(translationKey("advancement.", ".hammer_time.info"), "Bekomme ein Eisenhammer");
+            builder.add(translationKey("advancement.", ".hammer_collection.title"), "Hammer Kollektion");
+            builder.add(translationKey("advancement.", ".hammer_collection.info"), "Bekomme jeden Hammer");
+
+            // Enchantments
+            builder.addEnchantment(ModEnchantments.HAMMERING, "Hämmern");
+            builder.add(translationKey("enchantment.", ".hammering.desc"), "Gibt dir die Möglickeit, einen großen Bereich auf einmal abzubauen.");
+        }
+    }
+}
